013c0e6b44f2761f6cea6601d09233c69553a13a
Sam Watson <sambenwatson@gmail.com>
2024-08-19 12:24:08 +1000

    Added description to Section component for storybook

--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -9,6 +9,7 @@ interface ISectionProps {
 /**
  * Behaves like an accordion.
  * Inspired by the W3 example: https://www.w3.org/WAI/ARIA/apg/patterns/accordion/examples/accordion/
+ * Used in junction with the `SectionHeader` and `SectionContent` components.
  */
 export function Section({ children, initExpanded = false }: ISectionProps) {
   return (
